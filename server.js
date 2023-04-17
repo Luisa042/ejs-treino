fc9b03217f9da0e2f5228e39fc88a26c1dde4641
Luisa <luisa.santos042@gmail.com>
2023-04-17 23:22:13 +0000

    refactor: add directories 'pages' and 'partials'

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const server = express()
 server.set('view engine', 'ejs')
 
 server.get('/', (req, res) => {
-    res.render('index')
+    res.render('pages/index')
 })
 
 server.get('/sobre', (req,res) => {
-    res.render('about')
+    res.render('pages/about')
 })
 
 server.listen(8080, () => {console.log('server running on port 8080')})
