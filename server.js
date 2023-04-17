d9c2392ab8a0483988a97a16ffb432be5051b1d6
Luisa <luisa.santos042@gmail.com>
2023-04-17 23:48:44 +0000

    add array

--- server.js
+++ server.js
@@ -4,6 +4,32 @@ const server = express()
 server.set('view engine', 'ejs')
 
 server.get('/', (req, res) => {
+    const items = [
+        {
+            title: "D",
+            message: "Desenvolver aplicações e serviços de forma simples"
+        },
+        {
+            title: "E",
+            message: "EJS usa JavaScript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "Muito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "Aprovado!"
+        },
+        {
+            title: "I",
+            message: "install EJS"
+        },
+        {
+            title: "S",
+            message: "Sintaxe simples"
+        },
+    ];
     res.render('pages/index')
 })
 
